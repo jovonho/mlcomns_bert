33c6632cbd9a4bd2849eb0657c4875407d7dfe83
Loic Ho-Von <loic.hovon@gmail.com>
2022-08-02 21:09:05 -0400

    Add start_training script for easy tracing

---
+++ start_training.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#TODO: Parametrize num_gpus and mount points
+
+docker run -it --gpus all -v /data/bert/wiki:/wiki -v /data/bert/raw-data:/raw-data -v /raid/data/bert/preproc_data:/data -v /raid/data/bert/output:/output tf-bert:latest /bin/bash train_model.sh
