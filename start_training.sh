a2c397d7b65dfe79f09fedc2d4f72608ebebcc6a
Loic Ho-Von <loic.hovon@gmail.com>
2022-08-02 21:47:45 -0400

    start training script

--- start_training.sh
+++ start_training.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 #TODO: Parametrize num_gpus and mount points
-
-docker run -it --gpus all -v /data/bert/wiki:/wiki -v /data/bert/raw-data:/raw-data -v /raid/data/bert/preproc_data:/data -v /raid/data/bert/output:/output tf-bert:latest /bin/bash train_model.sh
+docker run -it --rm --gpus all -v /data/bert/wiki:/wiki -v /data/bert/raw-data:/raw-data -v /raid/data/bert/preproc_data:/data -v /raid/data/bert/output:/output tf-bert:latest /bin/bash train_model.sh
