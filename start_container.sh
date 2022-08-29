5e9a8c2e4a9143780b355737cb9b2b20930b1901
jovonho <loic.hovon@gmail.com>
2022-08-28 20:51:36 -0400

    Rename docker container to train_bert

--- start_container.sh
+++ start_container.sh
@@ -1,3 +1,3 @@
 #/bin/bash
 
-docker run -it --gpus all -v /data/bert/wiki:/wiki -v /data/bert/raw-data:/raw-data -v /raid/data/bert/preproc_data:/data -v /raid/data/bert/output:/output tf-bert:latest
+docker run -it --name=train_bert --gpus all -v /data/bert/wiki:/wiki -v /data/bert/raw-data:/raw-data -v /raid/data/bert/preproc_data:/data -v /raid/data/bert/output:/output tf-bert:latest
